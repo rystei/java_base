7ba988355897ad0be6fd5850b37125228c8291d5
rystei <gustavo.telles@edu.unifil.br>
2025-05-27 12:48:55 -0300

    Variaveis e tipos: Declarando Conversao de tipos

---
+++ fundamentos/VariaveisETipos/src/ConversaoTipos.java
@@ -0,0 +1,7 @@
+public class ConversaoTipos {
+    public static void main(String[] args) {
+        double valorDouble = 19.5;
+        int conversao = (int) valorDouble;
+        System.out.println("O valor inteiro do produto é: " + conversao);
+    }
+}
