c29b2163a1593db2f46709e54cbb2db4b50c7d18
rystei <gustavo.telles@edu.unifil.br>
2025-05-27 12:06:52 -0300

    Variaveis e tipos: Declarando valores e variáveis

---
+++ fundamentos/VariaveisETipos/src/DeclarandoVariaveis.java
@@ -0,0 +1,14 @@
+public class DeclarandoVariaveis {
+    public static void main(String[] args) {
+        String nome = "Maria";
+        int idade = 25;
+        double altura = 1.68;
+        boolean ehEstudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante: " + ehEstudante);
+
+    }
+}
