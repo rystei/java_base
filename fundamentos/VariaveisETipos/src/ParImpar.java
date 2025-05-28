9526c48a5da80e8d5b2d24ac33c02ea1858ea55d
rystei <gustavo.telles@edu.unifil.br>
2025-05-28 10:39:51 -0300

    Variaveis e tipos: Número é Par ou Impar

---
+++ fundamentos/VariaveisETipos/src/ParImpar.java
@@ -0,0 +1,13 @@
+public class ParImpar {
+    public static void main(String[] args) {
+        int numero = 7;
+
+        if (numero % 2 == 0) {
+            System.out.println("É par");
+        } else {
+            System.out.println("É impar");
+        }
+
+
+    }
+}
