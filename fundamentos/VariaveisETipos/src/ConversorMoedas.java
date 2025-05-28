f7be2f473b2c36e984104fc9ec45e954fbcb1906
rystei <gustavo.telles@edu.unifil.br>
2025-05-28 10:46:00 -0300

    Variaveis e tipos: Conversor de moedas real -> dolar

---
+++ fundamentos/VariaveisETipos/src/ConversorMoedas.java
@@ -0,0 +1,8 @@
+public class ConversorMoedas {
+    public static void main(String[] args) {
+        double valorReais = 451.50;
+        double cambio = 5.25;
+
+        System.out.println("O valor em dólares é: US$ " + valorReais / cambio);
+    }
+}
