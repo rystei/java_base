18ab5b533d5ddb44aa00c04ccc8db4d9ebe5cb1d
rystei <gustavo.telles@edu.unifil.br>
2025-05-30 12:24:53 -0300

    if e else: aula intodutoria

--- fundamentos/Condicionais/src/Main.java
+++ fundamentos/Condicionais/src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // - Condicionais (if, else if, else) ajudam a decidir qual código executar.
+        // - Usamos 'if' para verificar uma condição e 'else' para o caso contrário.
+        // - Operadores de comparação: >, <, ==, !=, >=, <=.
+        // - Operadores lógicos: AND (&&) e OR (||) para combinar condições.
+
+        String nome = "rystei";
+        double salario = 2756.90;
+        int numeroDependentes = 2;
+        boolean isento = true;
+
+        if (salario > 2523.45 && !isento) {
+            double irff = salario / 100 * 7.5;
+            System.out.printf("Valor IRRF: %.2f%n", irff);
+        } else if (isento) {
+            System.out.println("Contribuinte isento de IRRF.");
+        } else {
+            System.out.println("Não tem valor de IRRF.");
         }
     }
 }
