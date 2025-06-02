974f9ad3e550497618c54199559e746db3280dd7
rystei <gustavo.telles@edu.unifil.br>
2025-06-02 11:35:44 -0300

    if e else: Verificação de dia útil

---
+++ fundamentos/Condicionais/src/DiaUtil.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DiaUtil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o dia da semana: ");
+        String diaSemana = sc.nextLine().toLowerCase();
+        sc.close();
+
+        if (diaSemana.equals("segunda") || diaSemana.equals("terca") ||
+                diaSemana.equals("quarta") || diaSemana.equals("quinta") ||
+                diaSemana.equals("sexta")) {
+            System.out.println(diaSemana + " é um dia útil.");
+        } else {
+            System.out.println(diaSemana + " não é um dia útil.");
+        }
+    }
+}
