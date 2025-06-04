9422e1655e0a84971114866e2c8177b96c3121c9
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-04 10:21:13 -0300

    laços for e while: introdução

--- fundamentos/lacos/src/Main.java
+++ fundamentos/lacos/src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Declaração de arrays para armazenar os nomes dos vendedores e seus respectivos valores de vendas
+        String[] vendedores = {"Carlos", "Mariana", "João", "Fernanda"};
+        double[] vendas = {4000.00, 8000.00, 12000.00, 5000.00};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Laço de repetição 'for' para iterar sobre os vendedores
+        // Neste caso, estamos ignorando o vendedor 'Mariana' usando a instrução 'continue'
+        for (int i = 0; i < vendedores.length; i++) {
+            if (vendedores[i].equalsIgnoreCase("Mariana")){
+                continue;
+            }
+            // Imprime o nome do vendedor e sua respectiva comissão
+            System.out.printf("%s - comissão %.2f\n", vendedores[i], calcularComissao(vendas[i]));
+        }
+
+        // Teste usando o laço de repetição 'while'
+        System.out.println("\n Teste usando while");
+        int j = 0;
+        boolean imprimeOutro = true;
+
+        // Criação de um objeto Scanner para ler a entrada do usuário
+        Scanner sc = new Scanner(System.in);
+
+        // Laço de repetição 'do-while' para imprimir os vendedores e perguntar se o usuário deseja imprimir outro
+        do {
+            System.out.printf("%s - comissão %.2f\n", vendedores[j], calcularComissao(vendas[j]));
+            j++;
+            System.out.println("Deseja imprimir outro?");
+            imprimeOutro = sc.nextBoolean();
+        } while (imprimeOutro);
+
+        // Imprime todos os nomes dos vendedores usando o laço de repetição 'for-each'
+        System.out.println("Todos os nomes");
+        for (String nomeVendedor: vendedores){
+            System.out.println("Nome" + nomeVendedor);
+        }
+
+    }
+
+    // Método para calcular a comissão de um vendedor com base no total de vendas
+    public static double calcularComissao(double totalVendas) {
+        if (totalVendas <= 5000) {
+            return totalVendas * 0.3;
+        } else if (totalVendas <= 10000) {
+            return totalVendas * 0.5;
+        } else {
+            return totalVendas * 0.7;
         }
     }
 }
